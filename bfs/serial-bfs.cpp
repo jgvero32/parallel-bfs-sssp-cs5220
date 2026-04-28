dd2f81aab32e643cc93dc31369288e29095be82d
“Jelena <jg2587@cornell.edu>
2026-04-27 21:31:08 -0400

    Add distances vector as return instead

--- bfs/serial-bfs.cpp
+++ bfs/serial-bfs.cpp
@@ -16,16 +16,16 @@ How to run:
 // BFS for single connected component
 std::vector<int> bfs(const Graph &g, int src) {
     std::vector<bool> visited(g.num_nodes, false);
-    std::vector<int> res;
+    std::vector<int> distances(g.num_nodes, -1);
     std::queue<int> queue;
 
     visited[src] = true;
     queue.push(src);
+    distances[src] = 0;
 
     while (!queue.empty()) {
         int curr = queue.front();
         queue.pop();
-        res.push_back(curr);
 
         // visit all the unvisited neighbours of curr node
         for (long index_for_col_ind = g.row_ptr[curr];
@@ -33,12 +33,13 @@ std::vector<int> bfs(const Graph &g, int src) {
             int neighbor = g.col_ind[index_for_col_ind];
             if (!visited[neighbor]) {
                 visited[neighbor] = true;
+                distances[neighbor] = distances[curr] + 1;
                 queue.push(neighbor);
             }
         }
     }
 
-    return res;
+    return distances;
 }
 
 std::vector<int> parallel_bfs(const Graph &g, int src) {
@@ -77,14 +78,14 @@ int main(int argc, char *argv[]) { // argv looks like {./bfs_serial,
     std::cout << "Running serial BFS from source node: " << source << std::endl;
     auto t0 = std::chrono::steady_clock::now();
 
-    std::vector<int> res = bfs(g, source);
+    std::vector<int> distances = bfs(g, source);
 
     auto t1 = std::chrono::steady_clock::now();
     double elapsed = std::chrono::duration<double>(t1 - t0).count();
 
     std::cout << "\n----- Serial BFS Results -----\n";
     std::cout << "  Source node      : " << source << std::endl;
-    std::cout << "  Nodes visited    : " << res.size() << std::endl;
+    std::cout << "  Nodes visited    : " << nodes_visited(distances) << std::endl;
     std::cout << "  Node ID space    : " << g.num_nodes << " (max_node_id + 1)"
               << std::endl;
     std::cout << "  Elapsed time     : " << elapsed << " seconds\n";
