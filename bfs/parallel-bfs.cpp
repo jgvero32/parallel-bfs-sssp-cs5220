34c9ce6137cbb568b5764b3c40e874ce8714a8b1
Alanna Zhang <az562@login02.chn.perlmutter.nersc.gov>
2026-04-29 12:15:27 -0700

    Formatting

--- bfs/parallel-bfs.cpp
+++ bfs/parallel-bfs.cpp
@@ -6,9 +6,9 @@ Parallel BFS for roadNet-CA
 How to run:
    Create an interactive session on Perlmutter
    From build/, run make
-   srun -N 1 --ntasks-per-node=<num processors> ./bfs_parallel ../datasets/<dataset>.txt <source node>
+   srun -N 1 --ntasks-per-node=<num processors> ./bfs_parallel
+../datasets/<dataset>.txt <source node>
 */
-
 
 #include "graph_utils.h"
 #include <chrono>
@@ -18,7 +18,6 @@ How to run:
 #include <queue>
 #include <string>
 #include <vector>
-
 
 // Stores the distances from source node
 static std::vector<int> dists;
@@ -30,240 +29,212 @@ static std::vector<int> displacements;
 static std::vector<int> rows_per_proc;
 static std::vector<int> result;
 
-
 /**
-*
-*/
+ *
+ */
 void initialize(const Graph &graph, int source, int rank, int num_procs) {
-   if (rank >= graph.num_nodes) return;
+    if (rank >= graph.num_nodes)
+        return;
 
+    dists.reserve(graph.num_nodes);
+    frontier.reserve(graph.num_nodes);
+    for (int i = 0; i < graph.num_nodes; ++i) {
+        dists.push_back(-1); // Signifies a node hasn't been visited
+        frontier.push_back(false);
+    }
 
-   dists.reserve(graph.num_nodes);
-   frontier.reserve(graph.num_nodes);
-   for (int i = 0; i < graph.num_nodes; ++i) {
-       dists.push_back(-1); // Signifies a node hasn't been visited
-       frontier.push_back(false);
-   }
+    // TODO: load-balancing nodes based on number of outgoing edges
+    // Divide nodes evenly between processors
+    int base_rows = graph.num_nodes / num_procs;
+    int remainder_rows =
+        graph.num_nodes % num_procs; // Number of ranks with extra row
+    displacements.reserve(num_procs);
+    rows_per_proc.reserve(num_procs);
 
+    int disp = 0;
+    for (int i = 0; i < num_procs; ++i) {
+        displacements.push_back(disp);
+        if (i < remainder_rows) {
+            disp += base_rows + 1;
+            rows_per_proc.push_back(base_rows + 1);
+        } else {
+            disp += base_rows;
+            rows_per_proc.push_back(base_rows);
+        }
+    }
 
-   // TODO: load-balancing nodes based on number of outgoing edges
-   // Divide nodes evenly between processors
-   int base_rows = graph.num_nodes / num_procs;
-   int remainder_rows = graph.num_nodes % num_procs; // Number of ranks with extra row
-   displacements.reserve(num_procs);
-   rows_per_proc.reserve(num_procs);
+    start_row = displacements[rank];
+    if (displacements.size() - 1 == rank) {
+        end_row = graph.num_nodes;
+    } else {
+        end_row = displacements[rank + 1];
+    }
 
-
-   int disp = 0;
-   for (int i = 0; i < num_procs; ++i) {
-       displacements.push_back(disp);
-       if (i < remainder_rows) {
-           disp += base_rows + 1;
-           rows_per_proc.push_back(base_rows + 1);
-       } else {
-           disp += base_rows;
-           rows_per_proc.push_back(base_rows);
-       }
-   }
-
-
-   start_row = displacements[rank];
-   if (displacements.size() - 1 == rank) {
-       end_row = graph.num_nodes;
-   } else {
-       end_row = displacements[rank+1];
-   }
-
-
-   // Create the first frontier
-   if (start_row <= source && source < end_row) {
-       frontier[source] = true;
-       dists[source] = 0;
-   }
+    // Create the first frontier
+    if (start_row <= source && source < end_row) {
+        frontier[source] = true;
+        dists[source] = 0;
+    }
 }
-
 
 void parallel_bfs(const Graph &g, int rank, int num_procs) {
-   if (rank >= g.num_nodes) return;
+    if (rank >= g.num_nodes)
+        return;
 
+    std::vector<int> new_frontier;
+    new_frontier.resize(g.num_nodes);
+    new_frontier.assign(g.num_nodes, 0);
+    std::vector<int> recv_vals;
+    recv_vals.resize((end_row - start_row) * num_procs);
+    int displacements_rank[num_procs];
+    int row_ct_per_rank[num_procs];
+    // #pragma omp for parallel
+    for (int i = 0; i < num_procs; ++i) {
+        displacements_rank[i] = (end_row - start_row) * i;
+        row_ct_per_rank[i] = end_row - start_row;
+    }
 
-   std::vector<int> new_frontier;
-   new_frontier.resize(g.num_nodes);
-   new_frontier.assign(g.num_nodes, 0);
-   std::vector<int> recv_vals;
-   recv_vals.resize((end_row-start_row)*num_procs);
-   int displacements_rank[num_procs];
-   int row_ct_per_rank[num_procs];
-   // #pragma omp for parallel
-   for (int i = 0; i < num_procs; ++i) {
-       displacements_rank[i] = (end_row-start_row)*i;
-       row_ct_per_rank[i] = end_row-start_row;
-   }
+    int distance = 1;
+    while (true) {
+        // #pragma omp for parallel
+        for (int row = start_row; row < end_row; ++row) {
+            if (frontier[row]) {
+                for (int index = g.row_ptr[row]; index < g.row_ptr[row + 1];
+                     ++index) {
+                    // Add all the neighbors of this node to the new frontier
+                    new_frontier[g.col_ind[index]] = 1;
+                }
+            }
+        }
 
+        MPI_Alltoallv(new_frontier.data(), rows_per_proc.data(),
+                      displacements.data(), MPI_INT, recv_vals.data(),
+                      row_ct_per_rank, displacements_rank, MPI_INT,
+                      MPI_COMM_WORLD);
 
+        int has_frontier = 0;
+        // #pragma omp for parallel
+        for (int row = start_row; row < end_row; ++row) {
+            frontier[row] = false;
+            for (int i = 0; i < num_procs; ++i) {
+                int offset = row - start_row;
+                // Found unvisited node --> add to local frontier
+                if (recv_vals[i * (end_row - start_row) + offset] == 1 &&
+                    dists[row] == -1) {
+                    frontier[row] = true;
+                    dists[row] = distance;
+                    has_frontier = 1;
+                    break;
+                }
+            }
+        }
 
+        // Check if any new frontier is non-zero
+        int frontier_nodes;
+        MPI_Allreduce(&has_frontier, &frontier_nodes, 1, MPI_INT, MPI_SUM,
+                      MPI_COMM_WORLD);
 
-   int distance = 1;
-   while (true) {
-       // #pragma omp for parallel
-       for (int row = start_row; row < end_row; ++row) {
-           if (frontier[row]) {
-               for (int index = g.row_ptr[row]; index < g.row_ptr[row + 1];
-                ++index) {
-                   // Add all the neighbors of this node to the new frontier
-                   new_frontier[g.col_ind[index]] = 1;
-               }
-           }
-       }
-
-
-       MPI_Alltoallv(new_frontier.data(), rows_per_proc.data(), displacements.data(),
-           MPI_INT, recv_vals.data(), row_ct_per_rank, displacements_rank,
-           MPI_INT, MPI_COMM_WORLD);
-
-
-       int has_frontier = 0;
-       // #pragma omp for parallel
-       for (int row = start_row; row < end_row; ++row) {
-           frontier[row] = false;
-           for (int i = 0; i < num_procs; ++i) {
-               int offset = row - start_row;
-               // Found unvisited node --> add to local frontier
-               if (recv_vals[i * (end_row - start_row) + offset] == 1 && dists[row] == -1) {
-                   frontier[row] = true;
-                   dists[row] = distance;
-                   has_frontier = 1;
-                   break;
-               }
-           }
-       }
-
-
-       // Check if any new frontier is non-zero
-       int frontier_nodes;
-       MPI_Allreduce(&has_frontier, &frontier_nodes, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-
-
-       if (frontier_nodes > 0) {
-           // Finished processing one layer of nodes, clear the "next" frontier
-           std::fill(new_frontier.begin(), new_frontier.end(), 0);
-           distance++;
-       } else {
-           break;
-       }
-   }
+        if (frontier_nodes > 0) {
+            // Finished processing one layer of nodes, clear the "next" frontier
+            std::fill(new_frontier.begin(), new_frontier.end(), 0);
+            distance++;
+        } else {
+            break;
+        }
+    }
 }
-
 
 /**
-* Gathers the partitions of the dists vector onto rank 0
-*/
+ * Gathers the partitions of the dists vector onto rank 0
+ */
 void gather_result(int num_nodes, int rank, int num_procs) {
-   if (rank >= num_nodes) return;
-   // TODO: can I just use a pointer to start index instead of making a new
-   // vector?
-   std::vector<int> rank_dists(dists.begin() + start_row,
-                               dists.begin() + end_row);
+    if (rank >= num_nodes)
+        return;
+    // TODO: can I just use a pointer to start index instead of making a new
+    // vector?
+    std::vector<int> rank_dists(dists.begin() + start_row,
+                                dists.begin() + end_row);
 
-
-   if (rank == 0) {
-       result.resize(num_nodes);
-       MPI_Gatherv(rank_dists.data(), rank_dists.size(), MPI_INT, result.data(),
-                   rows_per_proc.data(), displacements.data(), MPI_INT, 0,
-                   MPI_COMM_WORLD);
-   } else {
-       MPI_Gatherv(rank_dists.data(), rank_dists.size(), MPI_INT, NULL, NULL,
-                   NULL, MPI_INT, 0, MPI_COMM_WORLD);
-   }
+    if (rank == 0) {
+        result.resize(num_nodes);
+        MPI_Gatherv(rank_dists.data(), rank_dists.size(), MPI_INT,
+                    result.data(), rows_per_proc.data(), displacements.data(),
+                    MPI_INT, 0, MPI_COMM_WORLD);
+    } else {
+        MPI_Gatherv(rank_dists.data(), rank_dists.size(), MPI_INT, NULL, NULL,
+                    NULL, MPI_INT, 0, MPI_COMM_WORLD);
+    }
 }
-
 
 int main(int argc, char *argv[]) {
-   if (argc < 3) {
-       std::cerr << "Usage: " << argv[0] << " <graph_file> <source_node>"
-                 << std::endl;
-       return 1;
-   }
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <graph_file> <source_node>"
+                  << std::endl;
+        return 1;
+    }
 
+    int num_procs, rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-   int num_procs, rank;
-   MPI_Init(&argc, &argv);
-   MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    const std::string dataset_file_name = argv[1];
+    int source = std::stoi(argv[2]);
 
+    // Rank 0 loads the graph, then broadcasts it to other ranks
+    Graph g;
+    if (rank == 0) {
+        std::cout << "Loading graph from: " << dataset_file_name << std::endl;
+        try {
+            g = load_graph(dataset_file_name, false);
+        } catch (const std::exception &e) {
+            std::cerr << "Error loading the graph??" << std::endl;
+            return 1;
+        }
+    }
 
-   const std::string dataset_file_name = argv[1];
-   int source = std::stoi(argv[2]);
+    MPI_Bcast(&g.num_nodes, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&g.num_edges, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    g.row_ptr.resize(g.num_nodes + 1);
+    g.col_ind.resize(g.num_edges);
+    MPI_Bcast(g.row_ptr.data(), g.num_nodes + 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(g.col_ind.data(), g.num_edges, MPI_INT, 0, MPI_COMM_WORLD);
 
+    if (source < 0 || source >= g.num_nodes) {
+        std::cerr << "Source node " << source << " out of range [0, "
+                  << g.num_nodes << ")\n";
+        return 1;
+    }
 
-   // Rank 0 loads the graph, then broadcasts it to other ranks
-   Graph g;
-   if (rank == 0) {
-       std::cout << "Loading graph from: " << dataset_file_name << std::endl;
-       try {
-           g = load_graph(dataset_file_name, false);
-       } catch (const std::exception &e) {
-           std::cerr << "Error loading the graph??" << std::endl;
-           return 1;
-       }
-   }
+    if (rank == 0) {
+        std::cout << "Running parallel BFS from source node: " << source
+                  << std::endl;
+    }
 
+    auto t0 = std::chrono::steady_clock::now();
 
-   MPI_Bcast(&g.num_nodes, 1, MPI_INT, 0, MPI_COMM_WORLD);
-   MPI_Bcast(&g.num_edges, 1, MPI_INT, 0, MPI_COMM_WORLD);
-   g.row_ptr.resize(g.num_nodes + 1);
-   g.col_ind.resize(g.num_edges);
-   MPI_Bcast(g.row_ptr.data(), g.num_nodes + 1, MPI_INT, 0, MPI_COMM_WORLD);
-   MPI_Bcast(g.col_ind.data(), g.num_edges, MPI_INT, 0, MPI_COMM_WORLD);
+    initialize(g, source, rank, num_procs);
 
+    parallel_bfs(g, rank, num_procs);
 
-   if (source < 0 || source >= g.num_nodes) {
-       std::cerr << "Source node " << source << " out of range [0, "
-                 << g.num_nodes << ")\n";
-       return 1;
-   }
+    gather_result(g.num_nodes, rank, num_procs);
 
+    auto t1 = std::chrono::steady_clock::now();
+    double elapsed = std::chrono::duration<double>(t1 - t0).count();
 
-   if (rank == 0) {
-       std::cout << "Running parallel BFS from source node: " << source
-           << std::endl;
-   }
+    if (rank == 0) {
+        std::cout << "\n----- Parallel BFS Results -----\n";
+        std::cout << "  Source node      : " << source << std::endl;
+        std::cout << "  Nodes visited    : " << nodes_visited(result)
+                  << std::endl;
+        std::cout << "  Node ID space    : " << g.num_nodes
+                  << " (max_node_id + 1)" << std::endl;
+        std::cout << "  Elapsed time     : " << elapsed << " seconds\n";
 
+        print_distances(result, 50);
+    }
 
-   auto t0 = std::chrono::steady_clock::now();
+    MPI_Finalize();
 
-
-   initialize(g, source, rank, num_procs);
-
-
-   parallel_bfs(g, rank, num_procs);
-
-
-   gather_result(g.num_nodes, rank, num_procs);
-
-
-   auto t1 = std::chrono::steady_clock::now();
-   double elapsed = std::chrono::duration<double>(t1 - t0).count();
-
-
-   if (rank == 0) {
-       std::cout << "\n----- Parallel BFS Results -----\n";
-       std::cout << "  Source node      : " << source << std::endl;
-       std::cout << "  Nodes visited    : " << nodes_visited(result) << std::endl;
-       std::cout << "  Node ID space    : " << g.num_nodes << " (max_node_id + 1)"
-               << std::endl;
-       std::cout << "  Elapsed time     : " << elapsed << " seconds\n";
-
-
-       print_distances(result, 50);
-   }
-
-
-   MPI_Finalize();
-
-
-   return 0;
+    return 0;
 }
-
-
-
