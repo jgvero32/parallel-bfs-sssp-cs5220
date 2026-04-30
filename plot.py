6990cb6e108c32a9bfdd06314145ae1e91f0767b
Alanna Zhang <az562@login02.chn.perlmutter.nersc.gov>
2026-04-29 17:22:27 -0700

    Updated plot function

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ import numpy as np
 plt.figure()
 plt.xlabel('Processor Count')
 plt.ylabel('Simulation Time (seconds)')
-plt.title('1D BFS Time on soc-LiveJournal1 vs Number of Processors')
+plt.title('1D Parallel BFS Time vs Number of Processors')
 
 x = [1, 2, 4, 8, 16, 32, 64]
 
@@ -22,10 +22,22 @@ ideal_liveJournal = [0.766427, 0.766427/64]
 
 plt.xscale("log", base=2)
 plt.yscale("log", base=2)
-plt.plot(x, roadNet, linestyle='-', color = "blue", label = "roadNet-CA")
-plt.plot(ideal_x, ideal_roadNet, color = "blue", linestyle="--", label = "ideal roadNet-CA")
-plt.plot(x, liveJournal, color = "orange", label = "soc-LiveJournal1")
-plt.plot(ideal_x, ideal_liveJournal, color = "orange", linestyle="--", label = "ideal soc-LiveJournal1")
+
+
+plt.axhline(y=0.109851,
+            color='purple',
+            linestyle='-',
+            linewidth=1.5,
+            label='Serial roadNet-CA')
+plt.axhline(y=0.526331,
+            color='red',
+            linestyle='-',
+            linewidth=1.5,
+            label='Serial soc-LiveJournal1')
+plt.plot(x, roadNet, linestyle='-', color = "blue", marker='s', markersize=4, label = "roadNet-CA")
+plt.plot(ideal_x, ideal_roadNet, color = "blue", linestyle="--", label = "Ideal roadNet-CA")
+plt.plot(x, liveJournal, color = "orange", marker='s', markersize=4, label = "soc-LiveJournal1")
+plt.plot(ideal_x, ideal_liveJournal, color = "orange", linestyle="--", label = "Ideal soc-LiveJournal1")
 plt.legend()
 
 out_fname = "plot.svg"
